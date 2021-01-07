ae270f73f7234cdae495340c9f69673ca9591781
bjorne84 <edinbjorn@gmail.com>
2021-01-07 15:02:41 +0100

    nu fungerar hela CRUD

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-// posts Model
+// posts Model, the schema-model
 const Posts = require("../../models/PostModel");
 
-// routes, api/posts
-
+// **** POST ****, routes, api/posts
 router.post("/", async (req, res) => {
   console.log(req.body);
   const newIcePost = new Posts({
@@ -14,9 +13,9 @@ router.post("/", async (req, res) => {
   });
   console.log(newIcePost);
   try {
-    // spara i db
+    // Save in DB
     const inPost = await newIcePost.save();
-    // Koll om försöket blir falskt att spara i databasen
+    // Check if it worked
     if (!inPost) {
       res.status(503);
       throw Error("Something went wrong while saving the post =( ");
@@ -25,9 +24,99 @@ router.post("/", async (req, res) => {
       res.status(201).json(inPost);
     }
   } catch (err) {
-    // dålig data in från användare = 400
-    res.status(400).json({ msg: err });
+    // Bad data in from user = 400
+    res.status(400).json({ msg: err.message });
   }
 });
+
+// **** GET ****, routes, api/posts
+// Get all posts
+router.get("/", async (req, res) => {
+  try {
+    const allPosts = await Posts.find();
+    if (!allPosts) {
+      throw Error("No items found");
+    } else {
+      res.status(200).json(allPosts);
+    }
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+});
+// GET one post
+router.get("/:id", getPost, (req, res) => {
+  res.json(res.post);
+});
+
+// UPDATE one post
+router.patch("/:id", getPost, async (req, res) => {
+  // options new = true makes mangoose send back updated data and not old
+  let options = { new: true };
+  try {
+    const post = await Posts.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      options
+    );
+    if (!post) {
+      throw Error("Something went wrong while updating the post =( ");
+    } else {
+      // It worked ok, post is created
+      res.status(201).json(post);
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Update version 2
+/*
+router.patch("/:id", async (req, res) => {
+  // options new = true makes mangoose send back updated data and not old
+  let options = { new: true };
+  try {
+    const post = await Posts.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      options
+    );
+    if (!post) {
+      throw Error("Something went wrong while updating the post =( ");
+    } else {
+      // It worked ok, post is created
+      res.status(201).json(post);
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+*/
+// DELETE one post
+router.delete("/:id", getPost, async (req, res) => {
+  try {
+    await res.post.deleteOne();
+    res.status(200).json({ message: "Success: Course is deleted!" });
+  } catch (err) {
+    res.status(503).json({ message: err.message });
+  }
+});
+
+// Creating middlewhere function to re-use, findbyid. Middlewhere idé = webdev simplified
+async function getPost(req, res, next) {
+  let post;
+  try {
+    post = await Posts.findById(req.params.id);
+    if (post == null) {
+      return res
+        .status(404)
+        .json({ message: "Cant find any post with that ID" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.post = post;
+  next();
+}
 
 module.exports = router;
